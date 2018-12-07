32517dec67361f18083798cb32ece7ec47a17bad
Thineskumar Thilakanathan <thineskumar_81@hotmail.com>
2018-12-08 00:05:13 +0100

    Initial

---
+++ ProductCatalog.API/Domains/CatalogType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductCatalog.API.Domains
+{
+    public class CatalogType
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
